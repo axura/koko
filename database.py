65d26b9cb5bcfc72b61c410292468517273d0eb7
Yue Kang <ykan215@cse.unsw.EDU.AU>
2014-10-22 15:47:19 +1100

    updated database.py. Have learnt how open a file and then amatch an expression. ideally want to be able to read multiline regex. to create a table in sqlite.

--- database.py
+++ database.py
@@ -4,10 +4,27 @@
 import sqlite3
 import os
 import re
+import glob
 
 #make connection
 conn = sqlite3.connect('students.db')
 #os.gedcwd()
+
+#step 2 setting up the fields
+cursor = conn.cursor()
+
+f = open("students/CrazyNinja82/profile.txt", "r")
+#print f.read()
+file_strings = str(f.read())
+print file_strings
+
+match = re.search("username:\n", file_strings)
+
+if match:
+	print match.group()
+
+
+f.close()
 
 
 #save what was created
