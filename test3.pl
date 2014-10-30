da52bca69f942c7db0d3b7a5d4afd7ab1d7a5993
Yue Kang <ykan215@cse.unsw.EDU.AU>
2014-10-30 16:52:58 +1100

    adding a new file test3.pl for testing retrieving every 10 users

---
+++ test3.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+# written by andrewt@cse.unsw.edu.au September 2013
+# as a starting point for COMP2041/9041 assignment 2
+# http://cgi.cse.unsw.edu.au/~cs2041/assignments/LOVE2041/
+
+use CGI qw/:all/;
+use CGI::Carp qw(fatalsToBrowser warningsToBrowser);
+use Data::Dumper;  
+use List::Util qw/min max/;
+use DBI;
+warningsToBrowser(1);
+
+my @display_fields = ("name","gender", "height", "birthdate","weight", "degree", "favourite_hobbies", "favourite_books","favourite_TV_shows", "favourite_movies", "favourite_bands");
+
+my $driver   = "SQLite";
+my $database = "students.db";
+my $dsn = "DBI:$driver:dbname=$database";
+my $userid = "";
+my $password = "";
+my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 })
+                      or die $DBI::errstr;
+
+my @students = glob("$students_dir/*");
+
+
